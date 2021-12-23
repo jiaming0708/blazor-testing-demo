d64a7d35778e9f26175b135da020b3ae6f5f8e79
Jimmy <jiaming0708@gmail.com>
2021-12-23 22:06:01 +0800

    compare shapshot

--- test/CounterTest.cs
+++ test/CounterTest.cs
@@ -26,7 +26,7 @@ public class CounterTest
 
     StringAssert.Contains("Current count: 1", comp.Markup);
   }
-  
+
   [Test]
   public void CounterShouldIncrementWhenClicked_CompareWithFirst()
   {
@@ -38,5 +38,23 @@ public class CounterTest
     var diffs = comp.GetChangesSinceFirstRender();
     var diff = diffs.ShouldHaveSingleChange();
     diff.ShouldBeTextChange("Current count: 1");
+  }
+  
+  [Test]
+  public void CounterShouldIncrementWhenClicked_CompareWithSnapshot()
+  {
+    using var ctx = new Bunit.TestContext();
+
+    var comp = ctx.RenderComponent<Counter>();
+    var button = comp.Find("button");
+    button.Click();
+
+    comp.SaveSnapshot();
+
+    button.Click();
+
+    var diffs = comp.GetChangesSinceSnapshot();
+    var diff = diffs.ShouldHaveSingleChange();
+    diff.ShouldBeTextChange("Current count: 2");
   }
 }
