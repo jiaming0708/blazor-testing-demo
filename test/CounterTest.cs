6d11eeba5f1a45483cd36bb9ded7ffc150a70778
Jimmy <jiaming0708@gmail.com>
2021-12-23 22:00:39 +0800

    first test case

--- test/CounterTest.cs
+++ test/CounterTest.cs
@@ -1,7 +1,17 @@
+using System;
+using client.Pages;
 using NUnit.Framework;
 
 namespace test;
 
 public class CounterTest
 {
+  [Test]
+  public void CounterRender()
+  {
+    using var ctx = new Bunit.TestContext();
+
+    var comp = ctx.RenderComponent<Counter>();
+    StringAssert.Contains("Current count: 0", comp.Markup);
+  }
 }
