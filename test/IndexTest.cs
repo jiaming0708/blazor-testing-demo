cfaa4a6ebd2487cdf8d813004dc3f193d8ab4ab8
Jimmy <jiaming0708@gmail.com>
2022-01-21 22:15:00 +0800

    use MOQ to mock component

--- test/IndexTest.cs
+++ test/IndexTest.cs
@@ -2,6 +2,7 @@ using System;
 using Bunit;
 using Bunit.TestDoubles;
 using client.Shared;
+using Moq;
 using NUnit.Framework;
 using Index = client.Pages.Index;
 
@@ -40,5 +41,19 @@ public class IndexTest
 
     var comp = ctx.RenderComponent<Index>();
     StringAssert.Contains("How is Blazor working for you?", comp.Markup);
+  }
+
+  [Test]
+  public void MockComponentByMOQ()
+  {
+    using var ctx = new Bunit.TestContext();
+    var mockComp = new Mock<SurveyPrompt>();
+    ctx.ComponentFactories.Add(mockComp.Object);
+    // 另一種寫法
+    // ctx.ComponentFactories.Add(() => Mock.Of<SurveyPrompt>());
+
+    var comp = ctx.RenderComponent<Index>();
+    var actualComp = comp.FindComponent<SurveyPrompt>();
+    Assert.AreSame(mockComp.Object, actualComp.Instance);
   }
 }
