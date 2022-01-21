a5821d91b00025b9f5b91376de0f3fcd5f4c3369
JimmyHo <jiaming0708@gmail.com>
2022-01-21 10:22:31 +0800

    mock http response for fetch page

--- test/FetchDataTest.cs
+++ test/FetchDataTest.cs
@@ -23,4 +23,21 @@ public class FetchDataTest
     var comp = ctx.RenderComponent<FetchData>();
     StringAssert.Contains("Loading...", comp.Markup);
   }
+
+  [Test]
+  public void RenderMockResponse()
+  {
+    using var ctx = new Bunit.TestContext();
+    var mock = ctx.Services.AddMockHttpClient();
+    mock.When("/sample-data/weather.json").RespondJson(new List<FetchData.WeatherForecast>
+    {
+      new() {Date = new DateTime(2022, 01, 20), TemperatureC = 15, Summary = "first data"}
+    });
+
+    var comp = ctx.RenderComponent<FetchData>();
+    comp.WaitForAssertion(() =>
+    {
+      Assert.IsNotNull(comp.Find(".table"));
+    });
+  }
 }
